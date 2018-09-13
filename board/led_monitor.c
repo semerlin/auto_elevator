f322ff016f8e2eb574dcdc29fd1627454b65f362
huang yang <elious.huang@gmail.com>
2018-09-13 21:19:02 +0800

    remove auto calculate logic

--- board/led_monitor.c
+++ board/led_monitor.c
@@ -27,10 +27,6 @@
 
 static uint16_t led_status = 0;
 
-bool floor_calculate = FALSE;
-
-#define ELEV_ERR_CNT   5
-
 /* led password */
 uint8_t led_pwd[5];
 
@@ -115,7 +111,6 @@ static void push_pwd_node(const pwd_node *node)
 static void vLedWorkMonitor(void *pvParameters)
 {
     char floor = 0;
-    uint8_t err_cnt = 0;
     for (;;)
     {
         /* check elevator status */
@@ -123,52 +118,9 @@ static void vLedWorkMonitor(void *pvParameters)
         {
             if (DEFAULT_CHECKIN != robot_checkin_get())
             {
-                if (!floor_calculate)
+                if (!is_led_on(floor) && (floor != elev_floor()))
                 {
-                    floor = floormap_phy_to_dis(robot_checkin_get());
-                    if (!is_led_on(floor) && (floor != elev_floor()))
-                    {
-                        /** check elevator status */
-                        if (run_up == elev_state_run())
-                        {
-                            if (floor > elev_floor())
-                            {
-                                elev_go(floor);
-                                err_cnt ++;
-                                if (err_cnt > ELEV_ERR_CNT)
-                                {
-                                    err_cnt = 0;
-                                    floor_calculate = TRUE;
-                                    elev_go(1);
-                                }
-                            }
-                        }
-                        else if (run_down == elev_state_run())
-                        {
-                            if (floor < elev_floor())
-                            {
-                                elev_go(floor);
-                                err_cnt ++;
-                                if (err_cnt > ELEV_ERR_CNT)
-                                {
-                                    err_cnt = 0;
-                                    floor_calculate = TRUE;
-                                    elev_go(1);
-                                }
-                            }
-                        }
-                        else
-                        {
-                            elev_go(floor);
-                            err_cnt ++;
-                            if (err_cnt > ELEV_ERR_CNT)
-                            {
-                                err_cnt = 0;
-                                floor_calculate = TRUE;
-                                elev_go(1);
-                            }
-                        }
-                    }
+                    elev_go(floor);
                 }
             }
         }
@@ -205,14 +157,6 @@ static void vLedMonitor(void *pvParameters)
                     if (is_floor_arrive(led_status, per_changed_bit))
                     {
                         TRACE("floor led off: %d\r\n", floor);
-                        if (floor_calculate)
-                        {
-                            if (0x01 == floor)
-                            {
-                                elev_set_first_floor();
-                                floor_calculate = FALSE;
-                            }
-                        }
                         /* notify floor arrived */
                         elev_arrived(floor);
                     }
